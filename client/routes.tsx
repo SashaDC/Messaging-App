9345550eafc48febd6bdb5a91cd1e7f3876a750f
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-20 12:09:11 +1300

    Added settings route.

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,9 +1,11 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App.tsx'
 import { ChatWindow } from './components/ChatWindow.tsx'
+import Settings from './components/Settings.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<ChatWindow />} />
+    <Route path="/settings" element={<Settings />} />
   </Route>,
 )
