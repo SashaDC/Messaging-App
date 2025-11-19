76a00ea45487d78ad1a2e74dc7d7fc62a53f4241
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-20 12:10:03 +1300

    Starting template for settings

---
+++ client/components/Settings.tsx
@@ -0,0 +1,14 @@
+// In here we'll edit and display stuff similar to discord like user data
+// CSS stuff maybe?
+export default function Settings() {
+  return (
+    <div className="flex min-h-screen bg-[#10002B] text-white">
+      <aside className="flex hidden flex-col border-r border-[#3C096C] bg-[#240046] p-4 md:flex md:w-64"></aside>
+      <main className="flex flex-1 flex-col">
+        <section className="flex-1 space-y-1 overflow-y-auto p-4"></section>
+
+        {/* Message input */}
+      </main>
+    </div>
+  )
+}
