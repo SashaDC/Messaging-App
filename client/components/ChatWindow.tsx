e7cfcc0c8f1c36ee29244c8097d889e69d5fbed1
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-20 12:14:11 +1300

    Moved logout to nav section

--- client/components/ChatWindow.tsx
+++ client/components/ChatWindow.tsx
@@ -113,22 +113,7 @@ export function ChatWindow() {
               />{' '}
             </svg>
           </Link>
-          <button className="ml-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
-              />
-            </svg>
-          </button>
+          <LogoutButton />
         </nav>
       </aside>
       {/* Main chat area */}
@@ -141,7 +126,6 @@ export function ChatWindow() {
             </h1>
             <p className="text-xs opacity-70">Chatting on DevConnect</p>
           </div>
-          <LogoutButton />
         </header>
 
         {/* Messages list */}
