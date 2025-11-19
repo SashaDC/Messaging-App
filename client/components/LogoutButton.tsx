eb2873d608cb6d276bafad58d85a724aed32ba20
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-20 12:13:56 +1300

    Stylized logout with icon instead

--- client/components/LogoutButton.tsx
+++ client/components/LogoutButton.tsx
@@ -7,11 +7,21 @@ export default function LogoutButton() {
     logout()
   }
   return (
-    <button
-      className="font-stretch-semi-expanded rounded-2xl border-2 border-[#fafafb] bg-[#10002B] font-semibold text-white hover:bg-[#3C096C] sm:min-h-12 sm:p-1 sm:text-sm md:min-h-16 md:p-2 md:text-base"
-      onClick={handleOnClick}
-    >
-      Sign out
+    <button className="ml-4" onClick={handleOnClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+        />
+      </svg>
     </button>
   )
 }
