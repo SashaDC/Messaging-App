f47259c4a3193c0055ef18ccbda01f7923e761e0
Jennifer Legg <jenn.e.legg@gmail.com>
2025-11-20 20:24:39 +1300

    formatting edit user

--- client/components/EditUserForm.tsx
+++ client/components/EditUserForm.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 interface FileSize {
   isTooBig: boolean
-  size: string
+  size: number
 }
 
 export default function EditUserForm({ currentUser, handleUpdateUser }: Props) {
@@ -26,7 +26,11 @@ export default function EditUserForm({ currentUser, handleUpdateUser }: Props) {
       : handleUpdateUser(formData)
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
     setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value })
   }
 
@@ -44,10 +48,10 @@ export default function EditUserForm({ currentUser, handleUpdateUser }: Props) {
   const checkFileSize = (file: File) => {
     console.log(file.size)
     if (file.size > 80000) {
-      setFileSize({ isTooBig: true, size: `${file.size / 1000}kb` })
+      setFileSize({ isTooBig: true, size: file.size })
     }
     if (file.size <= 80000) {
-      setFileSize({ isTooBig: false, size: `${file.size / 1000}kb` })
+      setFileSize({ isTooBig: false, size: file.size })
     }
   }
 
@@ -55,59 +59,71 @@ export default function EditUserForm({ currentUser, handleUpdateUser }: Props) {
     <form
       action="submit"
       onSubmit={handleSubmit}
-      className="flex flex-col items-center bg-[#10002B]"
+      className="flex min-h-screen flex-col items-center justify-center bg-[#240046]"
     >
-      <div className=" flex max-w-fit flex-col items-center border border-white bg-[#3C096C]  shadow-2xl shadow-white">
+      <div className="flex max-w-fit flex-col items-center border border-[#9D4EDD] bg-[#3C096C] px-10 py-4">
         {previewURL && (
           <img
             src={previewURL}
             alt="Preview your avatar"
-            className="m-4 max-h-52 max-w-52 rounded-full border-8 border-white"
+            className="duration-400 relative z-10 h-40 w-40 transform rounded-full border-4 border-[#E0AAFF]/80 object-cover shadow-xl transition-all hover:scale-105"
           />
         )}
-        <div>
-          <label htmlFor="pfp" className="m-4 bg-white">
-            Upload profile image:{' '}
-            <input
-              type="file"
-              name="pfp"
-              id="pfp"
-              accept="image/*"
-              onChange={handleFileChange}
-            />
+        <div className="m-4 text-center text-base text-white">
+          <label htmlFor="pfp" className="text-lg">
+            Upload profile image
           </label>
+          <input
+            type="file"
+            name="pfp"
+            id="pfp"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="block w-full pt-2"
+          />
           {fileSize && fileSize.isTooBig && (
-            <p>File is too large. Please choose an image under 80kb</p>
+            <p className=" text-red-700">
+              File is too large. <br /> Please choose an image under 80kb
+            </p>
           )}
-          {fileSize && !fileSize.isTooBig && <p>File size: {fileSize?.size}</p>}
+          {fileSize && !fileSize.isTooBig && (
+            <p className="text-green-500">File size: {fileSize?.size}</p>
+          )}
+        </div>
+        <div className="p-4">
+          <label htmlFor="username" className="m-4 text-lg text-white">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            id="username"
+            onChange={handleChange}
+            className="w-full border-2 border-black p-2 text-base text-black"
+            maxLength={255}
+          />
         </div>
 
-        <div>
-          <label htmlFor="bio">
-            Biography:{' '}
-            <input
-              type="text"
-              name="bio"
-              value={formData.bio ? formData.bio : ''}
-              id="bio"
-              onChange={handleChange}
-            />
+        <div className="p-4">
+          <label htmlFor="bio" className="m-4 text-lg text-white">
+            Biography
           </label>
+          <textarea
+            name="bio"
+            value={formData.bio ? formData.bio : ''}
+            id="bio"
+            onChange={handleChange}
+            className="h-44 w-full whitespace-normal text-wrap p-4 text-start text-base text-black"
+            maxLength={255}
+          />
         </div>
-        <div>
-          <label htmlFor="username">
-            Username:{' '}
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              id="username"
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className="transform rounded-full bg-gradient-to-r from-[#9D4EDD] to-[#C77DFF] px-8 py-3 font-medium text-black shadow-lg transition-all duration-300 hover:scale-105 hover:from-purple-600 hover:to-pink-600 hover:shadow-xl"
+        >
+          Submit
+        </button>
       </div>
     </form>
   )
