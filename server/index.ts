d49debc29767f4cd096caf789361d7cbe2e6eeb0
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-24 18:27:34 +1300

    Added future port for websocket testing.

--- server/index.ts
+++ server/index.ts
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', PORT)
+  // eslint-disable-next-line no-console
+  console.log('WebSocket available at ws://localhost:' + PORT)
 })
