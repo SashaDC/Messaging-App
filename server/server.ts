de0aa0887f7b5e832e5eae3dcf6d98ccf9cb55fe
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-24 18:28:08 +1300

    Renamed a few things to support the WebSocket stuff and referenced its handler.

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,33 @@
 import express from 'express'
+import { createServer } from 'http' // Websocket things
+import { WebSocketServer } from 'ws' // ----
 import * as Path from 'node:path'
 
+import { setupWebSocket } from './websocket/handlers.ts' // WebSocket Connection handler(for messages mainly)
 import usersRoutes from './routes/users.ts'
 import messagesRoutes from './routes/messages.ts'
 import relationshipRoutes from './routes/relationships.ts'
 
-const server = express()
+// Renamed server to app due to WebSocket weirdness
+const app = express()
+const server = createServer(app)
+const wss = new WebSocketServer({ server })
 
-server.use(express.json())
+app.use(express.json())
 
-server.use('/api/v1/users', usersRoutes)
-server.use('/api/v1/messages', messagesRoutes)
-server.use('/api/v1/relationships', relationshipRoutes)
+// Server Routes
+app.use('/api/v1/users', usersRoutes)
+app.use('/api/v1/messages', messagesRoutes)
+app.use('/api/v1/relationships', relationshipRoutes)
 
+// Sets up the WebSocket based on the handler file from the WebSocket folder
+setupWebSocket(wss)
+
+// Node_Enviroment stuff
 if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(Path.resolve('public')))
-  server.use('/assets', express.static(Path.resolve('./dist/assets')))
-  server.get('*', (req, res) => {
+  app.use(express.static(Path.resolve('public')))
+  app.use('/assets', express.static(Path.resolve('./dist/assets')))
+  app.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
   })
 }
