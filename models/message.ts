a5270cba7dfff3937daf57326000b54ee9002d37
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-25 21:25:49 +1300

    Emergency push, fixed models

--- models/message.ts
+++ models/message.ts
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 export interface MessageData {
-    friendshipId: number
-    senderId: string
-    message: string
-    createdAt: string | undefined
+  friendshipId: number
+  senderId: string
+  message: string
+  createdAt: string | undefined
 }
-=======
+
 export type Message = {
   id: number
   text: string
@@ -13,12 +12,9 @@ export type Message = {
   createdAt?: string
 }
 
-export interface MessageData {}
-
 // {
 //   friendship_id: 1,
 //   sender_id: 'auth0|123',
 //   message: 'Hi friend how are you?',
 //   created_at: knex.fn.now(),
 // },
->>>>>>> dev
