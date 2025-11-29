9d032751f38fc207d00c6214fe7dd0fa32bfb57f
Jennifer Legg <jenn.e.legg@gmail.com>
2025-11-29 13:49:33 +1300

    ph layout kinda working, but previous chat window shows when going to chat

--- client/components/BackArrow.tsx
+++ client/components/BackArrow.tsx
@@ -1,18 +1,28 @@
-export default function BackArrow() {
+interface Props {
+  handleClick: () => void
+}
+
+export default function BackArrow({ handleClick }: Props) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="size-6"
+    <button
+      onClick={handleClick}
+      aria-label="Return to previous page"
+      className="p-2"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
-      />
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+        />
+      </svg>
+    </button>
   )
 }
