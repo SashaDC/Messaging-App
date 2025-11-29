5b406487ee5ca27895e496f1daf5762810756afa
Paul <doomedrevier@gmail.com>
2025-11-30 10:44:46 +1300

    testing

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -11,36 +11,16 @@ export default {
     connection: {
       filename: Path.join(__dirname, 'dev.sqlite3'),
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
+    migrations: { directory: Path.join(__dirname, 'migrations') },
+    seeds: { directory: Path.join(__dirname, 'seeds') },
+    pool: { afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb) },
   },
-
   test: {
     client: 'sqlite3',
     useNullAsDefault: true,
-    connection: {
-      filename: ':memory:',
-    },
-    migrations: {
-      directory: Path.join(__dirname, 'migrations'),
-    },
-    seeds: {
-      directory: Path.join(__dirname, 'seeds'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-  },
-
-  production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: '/app/storage/prod.sqlite3',
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
+    connection: { filename: ':memory:' },
+    migrations: { directory: Path.join(__dirname, 'migrations') },
+    seeds: { directory: Path.join(__dirname, 'seeds') },
+    pool: { afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb) },
   },
 }
