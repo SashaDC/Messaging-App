16116870bdbf6908fdc6afe222c3d8c5eb6037a9
Sasha Chambers <sashachambers10.12@gmail.com>
2025-11-29 20:56:18 +1300

    updated message models to include images

--- models/message.ts
+++ models/message.ts
@@ -2,12 +2,14 @@ export interface MessageData {
   friendshipId: number
   senderId: string
   message: string
-  createdAt: string | undefined
+  image?: string // Using Base64 instead of multer as multer is more or less for single saving like icon updating.
+  createdAt: string
 }
 
 export type Message = {
   id: number
   text: string
+  image?: string // Using Base64 instead of multer as multer is more or less for single saving like icon updating.
   friendshipId: number
   sender: 'me' | 'them'
   createdAt: string
